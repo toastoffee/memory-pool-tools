11af2e41b53bb383a08f71903761f8046aa6ffa5
toastoffee <931391442@qq.com>
2024-07-04 20:30:06 +0800

    add implements

--- memory_pool.hpp
+++ memory_pool.hpp
@@ -25,7 +25,35 @@ private:
     void* _freeList;
     char* _pool;
     size_t _reminder;
+    long long _nums;
 };
+
+template<class T>
+MemoryPool<T>::MemoryPool(long long int nums) :
+_freeList(nullptr), _pool(nullptr), _reminder(0), _nums(nums){ }
+
+template<class T>
+T *MemoryPool<T>::New() {
+    T* allocate = nullptr;
+    if(_freeList)
+    {
+        allocate = (T*)_freeList;
+        _freeList = *(void**)_freeList;
+    }
+    else{
+        if( _reminder < sizeof(T)){
+            _reminder = sizeof(T) * _nums;
+            
+        }
+    }
+}
+
+template<class T>
+void MemoryPool<T>::Delete(T *obj) {
+    obj->~T();
+    *(void**)obj = _freeList;
+    _freeList = obj;
+}
 
 
 #endif //MEMORY_POOL_TOOLS_MEMORY_POOL_HPP
